6c2ba7d2230f1b6e8dd38401a6cf267504f652d7
qurtubi <qurtubi1411@gmail.com>
2024-06-02 21:59:48 +0700

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,24 @@
+// eslint.config.mjs
 import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import js from "@eslint/js";
 
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest, // Jika menggunakan Jest
+        ...globals.commonjs, // Untuk variabel global CommonJS seperti require dan module
+        // Tambahkan variabel global untuk CodeceptJS
+        Feature: "readonly",
+        Scenario: "readonly",
+        locate: "readonly",
+        actor: "readonly",
+      },
+      ecmaVersion: 2021,
+      sourceType: "module",
+    },
+  },
+  js.configs.recommended,
 ];
--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const ImageminWebpackPlugin = require("imagemin-webpack-plugin").default;
 const ImageminMozjpeg = require("imagemin-mozjpeg");
 const BundleAnalyzerPlugin =
